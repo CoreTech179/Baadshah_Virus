17becde0593168372dcdef5d69b8a51f22d412a3
CoreTech179 <141602614+CoreTech179@users.noreply.github.com>
2023-10-06 19:43:43 +0530

    Update Baadshah.bat

--- Baadshah.bat
+++ Baadshah.bat
@@ -4,6 +4,9 @@ start www.google.com
 start www.facebook.com
 start www.youtube.com
 start www.yahoo.com
-start www.google.com
+start notepad++
+start mspaint
+start word
+start calc
 start www.youtube.com
 goto :loop
